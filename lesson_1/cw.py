53cb1995ad3263037f41d4ecc30fa63787828769
luchanos <lukash_91@mail.ru>
2022-01-23 22:41:32 +0300

    init

--- lesson_1/cw.py
+++ lesson_1/cw.py
@@ -1,4 +1,4 @@
-from telebot import TeleBot
+from telebot import TeleBot, types
 from envparse import Env
 
 env = Env()
@@ -11,6 +11,17 @@ bot = TeleBot(TOKEN)
 @bot.message_handler(commands=["start"])
 def start(message):
     c = 1
+
+
+# todo luchanos сюда можно прикрутить эластик
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
 
 
 while True:
--- lesson_1/periodic_notifier.py
+++ lesson_1/periodic_notifier.py
@@ -1,9 +1,13 @@
 import requests
+from datetime import datetime
 
 from envparse import Env
 
 env = Env()
-TOKEN = env.str("TOKEN")
-ADMIN_CHAT_ID = env.str("ADMIN_CHAT_ID")
+TOKEN = env.str("TOKEN", default="2104221180:AAGmwLGyb1m8fTnePreXS5bruo_HaW2Vj_w")
+ADMIN_CHAT_ID = env.str("ADMIN_CHAT_ID", default="362857450")
 
-requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ADMIN_CHAT_ID}&text=завожу бота")
+current_datetime = datetime.now()
+msg = f"currend date and time: {current_datetime}"
+
+requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ADMIN_CHAT_ID}&text={msg}")
